0a05aec255adb0315210ba5a385a835a1d5dba98
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-02 22:35:09 -0500

    fancier static pages now leveraging layouts a bit smarter

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Gordo::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
+
   resources :orders
 
   resources :users
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Gordo App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Gordo App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Gordo | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Gordo | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Gordo | About Us")
+    end
+  end
+end
