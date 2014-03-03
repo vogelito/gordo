53f0ac71e7b8b05b9b7fc96ab1497cfabee99faa
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-02 22:44:56 -0500

    better string interpolation for the app's base title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Gordo" }
+
   describe "Home page" do
 
     it "should have the content 'Gordo App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Gordo | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Gordo | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -37,7 +39,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Gordo | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -50,7 +52,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Gordo | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
