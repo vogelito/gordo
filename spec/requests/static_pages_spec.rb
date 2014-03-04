e87faaa1c70469b952295fcb5f27f6883916ad12
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-04 16:05:04 -0500

    checking links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,4 +44,20 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
   end
+
+   it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title("Help"))
+    click_link "Contact"
+    expect(page).to have_title(full_title("Contact"))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title("Sign up"))
+    click_link "gordo app"
+    expect(page).to have_title(full_title(""))
+  end
+
 end
