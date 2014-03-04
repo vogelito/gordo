bcf85394dbbd9a99fe2d5ee6f59e5d830c0d737b
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-04 01:00:01 -0500

    better testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Gordo::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   resources :orders
 
