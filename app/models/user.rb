28b3297ea213d4b9b6d0c3f3d5ed8943e2fe292f
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-09 22:05:25 -0400

    fixing validations (should be run on save and not only on create)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   has_many :orders
 
   validates :name, presence: true
-  validates :email, presence:true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+  validates :email, presence:true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   validates :cellphone, presence:true, uniqueness:true, format: { with: /\A\+1\d{10}\z/, message: "enter 10 digits" }
 
-  before_validation(on: :create) do
+  before_validation do
     if attribute_present?("cellphone")
       # get rid of all non-numeric characters
       phone = cellphone.gsub(/[^0-9]/, "")
