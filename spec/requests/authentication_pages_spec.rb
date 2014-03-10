3b01e37c95d3d77a17d06004b6d26ba83455f7bb
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-10 03:44:31 -0400

    changing the layout links

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,9 +26,8 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
-  end
 
-  describe "with valid information" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email",    with: user.email.upcase
@@ -41,4 +40,5 @@ describe "Authentication" do
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
+  end
 end
