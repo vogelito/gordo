95d6d5fcf96433098e947a97f00667af27bbb2ec
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-10 00:10:43 -0400

    Add a test for email downcasing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,6 +58,16 @@ describe User do
     end
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
