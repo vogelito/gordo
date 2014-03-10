670c440aef507f5f7969a67b4b5fd6eecfb3da75
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-10 03:47:07 -0400

    sign-in upon sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to Gordo!"
       redirect_to @user
     else
