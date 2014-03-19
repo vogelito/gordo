4349cc8e5e06169f7a01d0269dc6f249d647af00
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-20 00:40:34 +0100

    updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,14 +37,11 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,18 @@ RSpec::Matchers.define :have_error_message do |message|
     expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
+end
