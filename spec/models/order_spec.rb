936113b07df79d5f81ab6f51b1201e9449905f0c
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-20 11:11:22 +0100

    very basic Order testing

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Order do
+
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    # This code is not idiomatically correct.
+    @order = Order.new(address: "1 Western Ave Unit 1305", user_id: user.id)
+  end
+
+  subject { @order }
+
+  it { should respond_to(:address) }
+  it { should respond_to(:user_id) }
+end
