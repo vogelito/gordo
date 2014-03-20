8a5df6095ad5ec3ed2c3cdfc93d507bec8a00825
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-20 11:18:45 +0100

    the first validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  validates :user_id, presence: true
   belongs_to :user
 
   validates :address, presence: true
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -12,4 +12,11 @@ describe Order do
 
   it { should respond_to(:address) }
   it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @order.user_id = nil }
+    it { should_not be_valid }
+  end
 end
