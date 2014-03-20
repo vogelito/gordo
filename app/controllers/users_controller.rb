a234cd879c2b08432a1ecaedf27d980555cae865
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-20 13:01:05 +0100

    simplify orders controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,13 +76,6 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def signed_out_user
       redirect_to(root_url) if signed_in?
     end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,13 @@ module SessionsHelper
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
   def sign_out
     current_user.update_attribute(:remember_token,
                                   User.hash(User.new_remember_token))
