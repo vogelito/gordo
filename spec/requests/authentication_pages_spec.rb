6c08f00a4eeaa23beffb3ca3aa075c0c04384475
Daniel Vogel <dvogel@cs.stanford.edu>
2014-03-24 11:05:12 -0400

    add drop down menu for admins to add and list food items

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,11 +43,21 @@ describe "Authentication" do
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+      it { should_not have_link('List Food Items',  href: food_items_path) }
+      it { should_not have_link('Add Food Item',    href: new_food_item_path) }
 
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
+    end
+
+    describe "with admin information" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before { valid_signin(admin) }
+
+      it { should have_link('List Food Items',  href: food_items_path) }
+      it { should have_link('Add Food Item',    href: new_food_item_path) }
     end
   end
 
